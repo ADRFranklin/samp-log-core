f3e181a09d5fb30a451ab931fd0be27f90a68ac4
michael <adrfranklin95@gmail.com>
2026-02-28 06:12:16 +0000

    refactor: added static linking

--- include/samplog/export.h
+++ include/samplog/export.h
@@ -1,6 +1,8 @@
 //NOTE: Passing "-fvisibility=hidden" as a compiler option to GCC is advised!
 
-#if defined _WIN32 || defined __CYGWIN__
+#if defined(LOGCORE_STATIC)
+# define DLL_PUBLIC
+#elif defined _WIN32 || defined __CYGWIN__
 # ifdef __GNUC__
 #  ifdef IN_LOGCORE_PLUGIN
 #   define DLL_PUBLIC __attribute__ ((dllexport))
